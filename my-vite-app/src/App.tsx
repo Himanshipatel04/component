31e5ca23b452acd94f3c8f2765806c073a98b97a
Himanshipatel04 <himanshipatel0409@gmail.com>
2024-09-05 16:32:25 +0530

    check

--- my-vite-app/src/App.tsx
+++ my-vite-app/src/App.tsx
@@ -1,12 +1,11 @@
-// import Card from "./components/Card"
+import Card from "./components/Card"
 
 function App() {
   
 
   return (
     <div>
-        {/* <Card/> */}
-        hello
+        <Card/>
     </div>
   )
 }
